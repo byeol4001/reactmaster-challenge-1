d305a11770138ba36fb91f8d9854029a3febb0ba
byeol <byeol4001@gmail.com>
2022-03-02 20:26:29 +0900

    refactor: style 분리

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,41 +1,5 @@
 import { Link } from "react-router-dom";
-import styled from "styled-components";
-
-const Container = styled.div`
-  padding: 0px 20px;
-`;
-
-const Header = styled.header`
-  height: 15vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const CoinsList = styled.ul``;
-
-const Coin = styled.li`
-  background-color: white;
-  color: ${(props) => props.theme.bgColor};
-  border-radius: 15px;
-  margin-bottom: 10px;
-  a {
-    padding: 20px;
-    transition: color 0.2s ease-in;
-    display: block;
-  }
-  &:hover {
-    a {
-      color: ${(props) => props.theme.accentColor};
-    }
-  }
-`;
-
-const Title = styled.h1`
-  font-size: 48px;
-  color: ${(props) => props.theme.accentColor};
-`;
-
+import { Container, Header, Title, CoinsList, Coin } from "../style/Style";
 const coins = [
   {
     id: "btc-bitcoin",
